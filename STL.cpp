ea52a1d1b73b448fa0e6bcdb3c686c52695dfe8d
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2024-01-02 22:35:48 +0530

    Add files via upload

---
+++ STL.cpp
@@ -0,0 +1,17 @@
+/*
+STL mainly contains four parts: 
+1. Containers : Pairs, Vectors, Stack, Queue, Dequeue,Priority Queue, List, Map, Unordered, Multi  
+2. Iterators : begin(), end(), rbegin(), rend() 
+3. Algorithms : sort(), 
+4. Functions 
+
+
+*/
+
+#include <iostream>
+using namespace std; 
+int main()
+{
+    
+    return 0;
+}
