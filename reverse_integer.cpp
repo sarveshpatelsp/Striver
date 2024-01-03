0888ec26365e74e9d0e3fd74e13b27fabe0a2c95
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2024-01-03 14:16:49 +0530

    Add files via upload
    It is an extraction of digits problem.

---
+++ reverse_integer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int reverse(int x)
+{
+    long reverse = 0;
+    while (x != 0)
+    {
+        int last = x % 10;
+        reverse = (reverse * 10) + last;
+        x = x / 10;
+    }
+    if (reverse > INT_MAX || reverse < INT_MIN)
+        return 0;
+    return (int)reverse;
+}
+
+int main()
+{
+    int x = 423;
+    cout << reverse(x);
+    return 0;
+}
