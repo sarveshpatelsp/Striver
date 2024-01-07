51a4f7abf791a2d0c5aba2573abddbe98c49c7f8
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2024-01-07 23:01:36 +0530

    Add files via upload

---
+++ palindrome_number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool palindrome(int n)
+{
+    int copy = n;
+    int rev = 0;
+    while (copy > 0)
+    {
+        int last = copy % 10;
+        rev = (rev * 10) + last;
+        copy = copy / 10;
+    }
+    if (rev == n)
+        return true;
+    return false;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << palindrome(n);
+    return 0;
+}
