071224ddbea7a606fa657841e0a0f0a91fe563e2
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2024-01-14 21:20:21 +0530

    Update STL.cpp

--- STL.cpp
+++ STL.cpp
@@ -1,17 +1,28 @@
 /*
 STL mainly contains four parts: 
-1. Containers : Pairs, Vectors, Stack, Queue, Dequeue,Priority Queue, List, Map, Unordered, Multi  
+1. Containers : pairs, vectors, stack, queue, dequeue, priority queue, list, map, unordered, multi, set 
 2. Iterators : begin(), end(), rbegin(), rend() 
 3. Algorithms : sort(), 
-4. Functions 
+4. Functions : No need to learn. 
 
+-> auto keyword automatically detects the desired data type used in place; 
+-> 
 
 */
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std; 
 int main()
 {
+    pair<int,int>p; 
+    cout << p.first << " " << p.second << endl; 
+    pair<int, pair<int,int>>pp; 
+    cout << pp.first << " " << p.second.first << " " << p.second.second << endl;
+    vector<int>v; 
+    for (auto it : v)
+        cin >> it ; 
+    for (auto it : v)
+        cout << it << " " ; 
     
     return 0;
 }
