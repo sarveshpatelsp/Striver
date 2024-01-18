49ba29b0f94cd307185554c00211e5881037a409
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2024-01-18 22:27:27 +0530

    Add files via upload

---
+++ check_array_sorted.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check_array(vector<int> &arr, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] >= arr[i - 1])
+        {
+        }
+        else
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cout << check_array(arr, n);
+    return 0;
+}
+
+// This problem have only Optimal solution
