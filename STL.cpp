167fe4334f029156462082d5bb0a82db2d088a7b
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2025-01-03 23:04:42 +0530

    Updated List and Deque in STL.cpp

--- STL.cpp
+++ STL.cpp
@@ -9,6 +9,10 @@ STL mainly contains four parts:
 -> push_back and emplace_back in vector both of them are similar, Push the values in vector.
 -> emplace_back are more faster than push_back. 
 -> emplace_back dont need a curly brackets to push the pair values but push_back needs. 
+-> insert() func in vecotrs is very costly, using it is not a good practice. 
+-> In iterator end() func means a one step ahead. 
+-> For vectors, SLL is maintained in internal func. 
+-> For list, DLL is maintained in internal func.
 -> 
 */
 
@@ -26,7 +30,8 @@ int main()
         cin >> it ; 
     for (auto it : v)
         cout << it << " " ; 
-    */
+   */
+   /*
     vector<int> v;
     v.push_back(4);
     v.emplace_back(5); 
@@ -39,5 +44,30 @@ int main()
     vector<int>v(5); // It will print 0 or garbage values five times 
     vector<int>v1(4 , 2);
     vector<int>v2(v1); // It will copy the v1 vector in v2
-    return 0;
+   */
+   /*
+   list<int> l;
+   l.push_back(2); // 2
+   l.emplace_back(4); // 2 4
+   l.push_front(5) // 5 2 4
+   l.emplace_front(7) // 7 5 2 4
+   // Rest func are same as vector like rbegin , rend , begin , end , clear , insert , size , swap
+   */
+   /*
+   deque<int>dq;
+   dq.push_back(2); // 2
+   dq.emplace_back(4); // 2 4 
+   dq.push_front(3);  // 3 2 4
+   dq.push_back(1);  // 3 2 4 1 
+   dq.emplace_front(6); // 6 3 2 4 1
+
+   dq.pop_front(); // 3 2 4 1
+   dq.pop_back();  // 3 2 4
+   
+   cout << dq.back(); // 4
+   cout << dq.front(); // 3
+   // Rest func are same as vector
+   */
+   
+   return 0;
 }
