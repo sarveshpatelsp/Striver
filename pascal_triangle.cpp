d2451e227d135da7cb81c198d570d32028d60cdc
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2025-01-17 22:56:49 +0530

    Create pascal_triangle.cpp

---
+++ pascal_triangle.cpp
@@ -0,0 +1,18 @@
+vector<int> generateRows(int row) {
+        long long ans = 1;
+        vector<int> ansRow;
+        ansRow.push_back(1);
+        for (int i = 1; i < row; i++) {
+            ans *= (row - i);
+            ans /= i;
+            ansRow.push_back(ans);
+        }
+        return ansRow;
+    }
+
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for (int i = 1; i <= numRows ; i++)
+            ans.push_back(generateRows(i));
+        return ans;
+    }
