b55681b820334570cd657f3b1a646efd8a7714d7
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2025-01-18 11:26:28 +0530

    Create majority_element_2.cpp

---
+++ majority_element_2.cpp
@@ -0,0 +1,16 @@
+vector<int> majorityElement(vector<int>& nums) {
+        vector<int>ans;
+        int n = nums.size();
+        int mini = (int)(n/3) + 1;
+        unordered_map<int,int>mp;
+        for (int i = 0 ; i < n ; i++)
+        {
+            mp[nums[i]]++;
+            if(mp[nums[i]] == mini)
+            ans.push_back(nums[i]);
+            if(ans.size() == 2) 
+            break;
+        }
+        sort(ans.begin() , ans.end());
+        return ans;
+    }
