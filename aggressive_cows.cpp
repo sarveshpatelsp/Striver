4fdf2432af60384a36649bafcc985cf844168d33
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2025-01-24 22:45:40 +0530

    Create aggressive_cows.cpp

---
+++ aggressive_cows.cpp
@@ -0,0 +1,29 @@
+bool f(vector<int> &stalls , int dis , int k)
+{
+    int cnt = 1 , last = stalls[0];
+    for(int i = 0 ; i < stalls.size() ; i++)
+    {
+      if (stalls[i] - last >= dis) {
+        cnt++;
+        last = stalls[i];
+      }
+      if (cnt >= k)
+        return true;
+    }
+    return false;
+}
+
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    sort(stalls.begin() , stalls.end());
+    int n = stalls.size();
+    int low = 1 , high = stalls[n-1] - stalls[0];
+    while(low <= high)
+    {
+        int mid = (low + high) / 2;
+        if(f(stalls , mid , k))
+        low = mid + 1;
+        else high = mid - 1;
+    }
+    return high;
+}
