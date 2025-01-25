be58b8361fbab1352354e8c06f4a9e69c541311d
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2025-01-25 21:01:54 +0530

    Create minimum_days_to_make_M_bouquets.cpp

---
+++ minimum_days_to_make_M_bouquets.cpp
@@ -0,0 +1,30 @@
+    bool possible(vector<int>& bloomDay, int mid, int m, int k) {
+        int cnt = 0;
+        int noOfB = 0;
+        for (int i = 0; i < bloomDay.size(); i++) {
+            if (bloomDay[i] <= mid)
+                cnt++;
+            else {
+                noOfB += (cnt / k);
+                cnt = 0;
+            }
+        }
+        noOfB += (cnt / k);
+        return noOfB >= m;
+    }
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        long long val = m * 1LL * k * 1LL;
+        if (val > bloomDay.size())
+            return -1;
+        int mini = *min_element(bloomDay.begin(), bloomDay.end());
+        int maxi = *max_element(bloomDay.begin(), bloomDay.end());
+        int low = mini, high = maxi;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (possible(bloomDay, mid, m, k))
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return low;
