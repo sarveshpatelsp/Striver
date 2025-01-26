7ca38df7661c4b5ad16cfea827826efd3c579105
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2025-01-26 23:18:59 +0530

    Create find_the_smallest_divisor.cpp

---
+++ find_the_smallest_divisor.cpp
@@ -0,0 +1,19 @@
+    int f(vector<int>& nums, int threshold, int mid) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += ceil((double)nums[i] / (double)mid);
+        }
+        return sum;
+    }
+
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low = 1, high = *max_element(nums.begin(), nums.end());
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (f(nums, threshold, mid) <= threshold)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
