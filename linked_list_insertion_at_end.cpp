d0e4b8e2b5443b9c1ea1d4651ddcc746bb1e4e13
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2025-02-03 23:42:48 +0530

    Create linked_list_insertion_at_end.cpp

---
+++ linked_list_insertion_at_end.cpp
@@ -0,0 +1,12 @@
+    Node *insertAtEnd(Node *head, int x) {
+        Node* newNode = new Node(x);
+        Node* temp = head;
+        if(head == NULL)
+        return newNode;
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        return head;
+    }
