83e7e916b121fda734db2f407c2a9d7feb10c2e0
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2025-02-04 22:10:09 +0530

    Create maximum_Ascending_Subarray_Sum.cpp

---
+++ maximum_Ascending_Subarray_Sum.cpp
@@ -0,0 +1,13 @@
+    int maxAscendingSum(vector<int>& nums) {
+        int maxSum = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i - 1] < nums[i])
+                sum += nums[i];
+            else
+                sum = nums[i];
+
+            maxSum = max(maxSum, sum);
+        }
+        return maxSum;
+    }
