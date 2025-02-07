5b6215b3985c56c1aac83cf9c5eb6217316fae12
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2025-02-07 22:07:30 +0530

    Create Find_pairs_with_given_sum_in_DLL.cpp

---
+++ Find_pairs_with_given_sum_in_DLL.cpp
@@ -0,0 +1,28 @@
+    Node* findTail(Node* head)
+    {
+        Node* temp = head;
+        while(temp->next!=NULL)
+        temp = temp->next;
+        return temp;
+    }
+    
+    vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target)
+    {
+       vector<pair<int, int>> ans;
+       if(head == NULL) return ans;
+       Node* left = head;
+       Node* right = findTail(head); 
+       while(left->data < right->data)
+       {
+           if(left->data + right->data == target)
+           {
+               ans.push_back({left->data , right->data});
+               left = left->next;
+               right = right->prev;
+           }
+           else if(left->data + right->data < target)
+           left = left->next;
+           else right = right->prev;
+       }
+       return ans;
+    }
