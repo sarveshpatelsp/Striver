540f460dc35c9fc49f230bd591396990419fbfe0
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2025-02-08 21:54:40 +0530

    Update middle_in_LL.cpp

--- middle_in_LL.cpp
+++ middle_in_LL.cpp
@@ -9,3 +9,29 @@
         }
         return slow;
     }
+/*
+    ListNode* middleNode(ListNode* head) {
+        if (head->next == NULL)
+            return head;
+        int cnt = 0;
+        ListNode* temp = head;
+        while (temp != NULL) {
+            cnt++;
+            temp = temp->next;
+        }
+        int midNode = (cnt / 2) + 1;
+        temp = head;
+        while (temp != NULL) {
+            midNode--;
+            if (midNode == 0)
+                break;
+            temp = temp->next;
+        }
+        return temp;
+    }
+*/
+int main(){
+    ListNode* head = NULL;
+    middleNode(head); 
+    return 0;
+}
