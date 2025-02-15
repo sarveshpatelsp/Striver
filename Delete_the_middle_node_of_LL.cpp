5c134d31cafb7e45dea3d18c6e528feccb79c683
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2025-02-15 21:51:13 +0530

    Create Delete_the_middle_node_of_LL.cpp

---
+++ Delete_the_middle_node_of_LL.cpp
@@ -0,0 +1,16 @@
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head == NULL || head->next == NULL) return NULL;
+        ListNode* prev = new ListNode(0);
+        prev->next = head;
+        ListNode* fast = head;
+        ListNode* slow = prev;
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* delNode = slow->next;
+        slow->next = slow->next->next;
+        delete(delNode);
+        return head;
+    }
